2fe8b51f01ade7d7f9a1fa95fa0d39a243b20430
sivasakthi <cvasakthi96@gmail.com>
2024-10-06 05:29:46 +0530

    minimal ui integrated

--- src/component/StringCalculator.tsx
+++ src/component/StringCalculator.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 export interface IStringCalculatorProps {}
 
 export function StringCalculator(props: IStringCalculatorProps) {
+  const [enteredString, setEnteredString] = useState<string>("");
+  const handleCalculate = () => {
+    alert("handle calculate");
+  };
+  const handleReset = () => {
+    setEnteredString("");
+  };
   return (
-    <div className="min-h-96 w-full max-w-md bg-white rounded shadow-lg">
-      StringCalculator
+    <div className="min-h-96 w-full max-w-md bg-white rounded shadow-lg p-5 space-y-3">
+      <input
+        type="text"
+        className="w-full h-10 rounded border"
+        value={enteredString}
+        onChange={(e) => {
+          setEnteredString(e.target.value);
+        }}
+      />
+      <div className="flex w-full justify-end gap-3">
+        <button
+          className="h-9 w-fit bg-blue-700 rounded text-sm text-white px-1 "
+          onClick={() => {
+            handleCalculate();
+          }}
+        >
+          Calculate
+        </button>
+        <button
+          className="h-9 w-fit bg-red-700 rounded text-sm text-white px-1 "
+          onClick={() => {
+            handleReset();
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
