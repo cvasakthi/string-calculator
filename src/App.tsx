107316866f640f755d98c9b67dca558418e9341d
sivasakthi <cvasakthi96@gmail.com>
2024-10-06 08:45:25 +0530

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { StringCalculator } from "./component/StringCalculator";
 function App() {
   return (
     <div className=" h-screen w-screen bg-slate-50">
+      <header className="h-fit w-full bg-slate-100 p-3 grid place-items-center">
+        <h5 className="text-3xl font-semibold text-blue-800 font-sans">
+          String Calculator
+        </h5>
+      </header>
       <div className="container h-full mx-auto grid place-items-center">
         <StringCalculator />
       </div>
